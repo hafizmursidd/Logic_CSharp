fe376f179d0fb3d065b15a2157e1691f36d57e5f
unknown <hafizmursiddd@gmail.com>
2023-01-11 14:02:57 +0700

    first commit

---
+++ Solution C Sharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Solution_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            TestHelloWord.hey();
+        }
+    }
+}
---
+++ Solution C Sharp/TestHelloWord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_C_Sharp
+{
+    internal class TestHelloWord
+    {
+        public static void hey() {
+            Console.WriteLine("hello world for another class");
+        }
+    }
+}
